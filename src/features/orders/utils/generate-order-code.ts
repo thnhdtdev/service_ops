c24bdf4f8ccdd877fe8a6d38f43e89f432fdf114
thnhdtdev <thnhdat.dev@gmail.com>
2026-07-03 09:27:01 +0700

    🗑️  chore(utils): optimize year formatting and random string gen

--- src/features/orders/utils/generate-order-code.ts
+++ src/features/orders/utils/generate-order-code.ts
@@ -1,10 +1,10 @@
 export function generateOrderCode() {
 	const now = new Date();
 
-	const year = now.getFullYear();
+	const year = now.getFullYear() % 100;
 	const month = String(now.getMonth() + 1).padStart(2, "0");
 	const day = String(now.getDate()).padStart(2, "0");
-	const random = crypto.randomUUID().slice(0, 8).toUpperCase();
+	const random = crypto.randomUUID().slice(0, 4).toUpperCase();
 
 	return `SO-${year}${month}${day}-${random}`;
 }
